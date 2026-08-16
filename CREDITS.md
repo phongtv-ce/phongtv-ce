9af2d8d5e9e7358e16eeba2780210f33efdce258
PhongTV <phongtv.ce@gmail.com>
2026-08-16 14:59:43 +0700

    feat: update version of node and get quotes from txt data

---
+++ CREDITS.md
@@ -0,0 +1,5 @@
+## Credits
+
+### Data
+
+- [Quotes data - Robatron](https://gist.github.com/robatron/a66acc0eed3835119817)
--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-const axios = require("axios");
 const fs = require("fs");
+const path = require("path");
 
-const getQuote = async () => {
+const QUOTES_PATH = path.join(__dirname, "data", "quotes.txt");
+const README_PATH = path.join(__dirname, "README.md");
+
+const getQuote = () => {
   try {
-    const { data } = await axios.get("https://quotes.rest/qod?language=en&quot");
-    const quote = data.contents.quotes[0].quote;
-    const author = data.contents.quotes[0].author;
+    const lines = fs
+      .readFileSync(QUOTES_PATH, "utf8")
+      .split(/\r?\n/)
+      .map((line) => line.trim())
+      .filter(Boolean);
+
+    if (lines.length === 0) {
+      console.error("No quotes found in data/quotes.txt");
+      return {};
+    }
+
+    const now = new Date();
+    const yearStart = Date.UTC(now.getUTCFullYear(), 0, 1);
+    const today = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate());
+    const dayOfYear = Math.round((today - yearStart) / 86400000);
+    const line = lines[dayOfYear % lines.length];
+    const separator = line.lastIndexOf("~");
+
+    const quote = separator === -1 ? line : line.slice(0, separator).trim();
+    const author =
+      separator === -1
+        ? "Anonymous"
+        : line.slice(separator + 1).trim() || "Anonymous";
+
+    if (!quote) return {};
 
     console.log("new quote", `"${quote}"`);
 
-    return {
-      quote,
-      author,
-    };
+    return { quote, author };
   } catch (err) {
     console.error(err.message);
     return {};
   }
 };
 
-const generate = async () => {
-  const { quote, author } = await getQuote();
+const generate = () => {
+  const { quote, author } = getQuote();
 
   if (!quote) return;
 
-  fs.writeFileSync("README.md", `_**${quote}**_\n\n${author}`);
+  fs.writeFileSync(README_PATH, `_**${quote}**_\n\n${author}`);
 };
 
 generate();
